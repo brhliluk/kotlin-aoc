73b1c58685af88869ecf0456a9fc7fda48a9d4ed
Lukáš Brhlík <lukas.brhlik@juicymo.cz>
2022-12-01 11:54:47 +0100

    D1P1 solution

--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,14 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+fun <T> List<T>.chunkBy(predicate: (T) -> Boolean): Sequence<List<T>> = sequence {
+    val currentGroup = mutableListOf<T>()
+    for (element in this@chunkBy) {
+        if (predicate(element)) {
+            yield(currentGroup.toList())
+            currentGroup.clear()
+        } else currentGroup += element
+    }
+    yield(currentGroup)
+}
