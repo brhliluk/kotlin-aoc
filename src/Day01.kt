fb6e5c9ebe8272e8af098f2d9004d0c4bcfb7f03
Lukáš Brhlík <lukas.brhlik@juicymo.cz>
2022-12-01 12:08:52 +0100

    D1P2 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,13 @@
 fun main() {
     fun part1(input: List<String>) = input.chunkBy { it.isEmpty() }.map {
-            caloryList -> caloryList.map { it.toInt() }.reduce { acc, string -> acc + string }
+            calloryList -> calloryList.map { it.toInt() }.reduce { acc, meal -> acc + meal }
         }.maxOrNull()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day01_test")
-//    check(part1(testInput) == 1)
+    fun part2(input: List<String>) = input.chunkBy { it.isEmpty() }.map {
+            calloryList -> calloryList.map { it.toInt() }.reduce { acc, meal -> acc + meal }
+    }.sortedDescending().take(3).reduce { acc, meal -> acc + meal }
 
     val input = readInput("Day01")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
