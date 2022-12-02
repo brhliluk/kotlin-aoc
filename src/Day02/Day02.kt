9bdffcda2a6afb405e9e8a12d88f435f46896956
Lukáš Brhlík <lukas.brhlik@juicymo.cz>
2022-12-02 12:44:07 +0100

    D2P2 results

--- src/Day02/Day02.kt
+++ src/Day02/Day02.kt
@@ -3,9 +3,11 @@ fun main() {
         MyMove.byLetter(it[2])!!.getFightResult(OponentMove.byLetter(it[0])!!) + MyMove.byLetter(it[2])!!.value
     }.reduce { acc, result -> acc + result }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>) = input.map {
+        val oponentMove = OponentMove.byLetter(it[0])!!
+        val myMove = FightResult.byLetter(it[2])!!.getRequiredMove(oponentMove)
+        myMove.getFightResult(oponentMove) + myMove.value
+    }.reduce { acc, result -> acc + result }
 
     val input = readInput("Day02")
     println(part1(input))
@@ -40,6 +42,32 @@ enum class MyMove(val letter: Char, val value: Int) {
                 OponentMove.PAPER -> 6
                 OponentMove.SCISSORS -> 3
             }
+        }
+    }
+
+    companion object {
+        fun byLetter(letter: Char) = values().firstOrNull { it.letter == letter }
+    }
+}
+
+enum class FightResult(val letter: Char) {
+    LOOSE('X'), DRAW('Y'), WIN('Z');
+
+    fun getRequiredMove(oponentMove: OponentMove) = when (this) {
+        WIN -> when (oponentMove) {
+            OponentMove.ROCK -> MyMove.PAPER
+            OponentMove.PAPER -> MyMove.SCISSORS
+            OponentMove.SCISSORS -> MyMove.ROCK
+        }
+        DRAW -> when (oponentMove) {
+            OponentMove.ROCK -> MyMove.ROCK
+            OponentMove.PAPER -> MyMove.PAPER
+            OponentMove.SCISSORS -> MyMove.SCISSORS
+        }
+        LOOSE -> when (oponentMove) {
+            OponentMove.ROCK -> MyMove.SCISSORS
+            OponentMove.PAPER -> MyMove.ROCK
+            OponentMove.SCISSORS -> MyMove.PAPER
         }
     }
 
