a1909b1c486dbcfb0a3c67a794b2440832b31dcc
Lukáš Brhlík <lukas.brhlik@juicymo.cz>
2022-12-05 09:55:17 +0100

    D4P2 late

--- src/Day04/Day04.kt
+++ src/Day04/Day04.kt
@@ -9,13 +9,15 @@ fun main() {
         firstElf.all { it in secondElf } || secondElf.all { it in firstElf }
     }
 
-    fun part2(input: List<String>) = input.map {
-        val oponentMove = OponentMove.byLetter(it[0])!!
-        val myMove = FightResult.byLetter(it[2])!!.getRequiredMove(oponentMove)
-        myMove.getFightResult(oponentMove) + myMove.value
-    }.reduce { acc, result -> acc + result }
+    fun part2(input: List<String>) = input.count { sections ->
+        val matchedResult = matcher.matchEntire(sections)!!
+        val (firstElfStart, firstElfEnd, secondElfStart, secondElfEnd) = matchedResult.destructured
+        val firstElf = firstElfStart.toInt()..firstElfEnd.toInt()
+        val secondElf = secondElfStart.toInt()..secondElfEnd.toInt()
+        (firstElf intersect secondElf).isNotEmpty()
+    }
 
     val input = readInput("Day04")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
