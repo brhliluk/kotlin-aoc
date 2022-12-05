e1fa7f00dfcd5cd508220e31ae1df6d0d9d3ca67
Lukáš Brhlík <lukas.brhlik@juicymo.cz>
2022-12-05 11:51:10 +0100

    D5P1 solution

--- src/Day05/Day05.kt
+++ src/Day05/Day05.kt
@@ -1,7 +1,25 @@
 fun main() {
-    fun part1(input: List<String>) = input.map {
-        MyMove.byLetter(it[2])!!.getFightResult(OponentMove.byLetter(it[0])!!) + MyMove.byLetter(it[2])!!.value
-    }.reduce { acc, result -> acc + result }
+    val matcher = Regex("move (\\d.*) from (\\d) to (\\d)")
+    //todo 2 cipher numbers
+    val _dock = Array(9) { Array<Char?>(8) { null } }
+
+    fun part1(input: List<String>) : CharArray {
+        // Parse input
+        for (i in 0..7) {
+            for (j in 1..34 step 4) {
+                with (input[i][j]) { if (this.isLetter()) _dock[j/4][7-i] = this }
+            }
+        }
+        val dock = _dock.map { it.filterNotNull().toMutableList() }
+        input.takeLast(input.size - 10).forEach {
+            val match = matcher.matchEntire(it)!!
+            val (count, from, to) = match.destructured
+            repeat (count.toInt()) {
+                dock[to.toInt()-1].add(dock[from.toInt()-1].removeLast())
+            }
+        }
+        return dock.map { it.last() }.toCharArray()
+    }
 
     fun part2(input: List<String>) = input.map {
         val oponentMove = OponentMove.byLetter(it[0])!!
@@ -9,7 +27,7 @@ fun main() {
         myMove.getFightResult(oponentMove) + myMove.value
     }.reduce { acc, result -> acc + result }
 
-    val input = readInput("Day02")
+    val input = readInput("Day05")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
