f7e47d4852103115ef57aa16e37a9a12ec70d33b
Lukáš Brhlík <lukas.brhlik@juicymo.cz>
2022-12-12 18:51:40 +0100

    D8P2 late solution

--- src/Day08/Day08.kt
+++ src/Day08/Day08.kt
@@ -1,5 +1,23 @@
 fun main() {
 
+    fun List<Int>.takeWhileInclusive(pred: (Int) -> Boolean): List<Int> {
+        var shouldContinue = true
+        return takeWhile {
+            val result = shouldContinue
+            shouldContinue = pred(it)
+            result
+        }
+    }
+
+    fun List<Int>.takeLastWhileInclusive(pred: (Int) -> Boolean): List<Int> {
+        var shouldContinue = true
+        return takeLastWhile {
+            val result = shouldContinue
+            shouldContinue = pred(it)
+            result
+        }
+    }
+
     fun <T> List<List<T>>.getColumn(index: Int): List<T> {
         val result = mutableListOf<T>()
         forEachIndexed { i, elemList ->
@@ -16,9 +34,18 @@ fun main() {
         return element > rightMax || element > leftMax
     }
 
-    fun isVisible(row: List<Int>, column: List<Int>, rowIdx: Int, columnIdx: Int): Boolean {
-        return isVisible(row, rowIdx) || isVisible(column, columnIdx)
+    fun isVisible(row: List<Int>, column: List<Int>, rowIdx: Int, columnIdx: Int) = isVisible(row, rowIdx) || isVisible(column, columnIdx)
+
+    fun scenicScore(row: List<Int>, index: Int): Int {
+        val tree = row[index]
+        val leftTrees = if (index == 0) listOf() else row.subList(0, index)
+        val rightTrees = if (index == row.size - 1) listOf() else row.subList(index + 1, row.size)
+        val leftVisible = if (leftTrees.isEmpty()) leftTrees else leftTrees.takeLastWhileInclusive { it < tree }
+        val rightVisible = if (rightTrees.isEmpty()) rightTrees else rightTrees.takeWhileInclusive { it < tree }
+        return leftVisible.size * rightVisible.size
     }
+
+    fun scenicScore(row: List<Int>, column: List<Int>, rowIdx: Int, columnIdx: Int) = scenicScore(row, rowIdx) * scenicScore(column, columnIdx)
 
     fun part1(input: List<String>): Int {
         val visibleTrees = mutableListOf<String>()
@@ -36,7 +63,16 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val scores = mutableListOf<Int>()
+        val array = input.map { row ->
+            row.toCharArray().map { it.digitToInt() }
+        }
+        array.forEachIndexed { i, trees ->
+            trees.forEachIndexed { j, tree ->
+                scores.add(scenicScore(trees, array.getColumn(j), j, i))
+            }
+        }
+        return scores.max()
     }
 
     val input = readInput("Day08")
